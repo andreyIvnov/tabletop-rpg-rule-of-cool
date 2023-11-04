5dd957c1863dab075c9220df10cc9bb7d9ced90f
Andrey Ivanov <68381704+andreyIvnov@users.noreply.github.com>
2023-11-04 16:56:27 +0200

    Creating a design of login page with Angular Material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,23 +7,5 @@ describe('AppComponent', () => {
     imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'tabletop-rpg-rule-of-cool'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tabletop-rpg-rule-of-cool');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tabletop-rpg-rule-of-cool app is running!');
-  });
+  
 });
