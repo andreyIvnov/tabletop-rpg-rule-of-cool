b38c204b8f8feb5bd208d936e2d776afcd58ead3
Andrey Ivanov <68381704+andreyIvnov@users.noreply.github.com>
2023-11-07 17:26:26 +0200

    Rederect to signin page like home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'signin' },
   {
     path: 'signin',
     loadChildren: () => import('./pages/signin/signin.module').
       then(m => m.SigninModule)
-  }
+  },
   
 ];
 
